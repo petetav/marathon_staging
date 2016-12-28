e8dbb675ef758c3a07e7c3f9015ec9ee51d7d7fd
petetav <petethepilot69@gmail.com>
2016-12-28 08:27:01 +0000

    first attempt

---
+++ good_causes.php
@@ -0,0 +1,91 @@
+<html>
+<head>
+<meta charset="utf-8">
+
+<title>Lingfield Marathon Fun Run 2014</title>
+ <meta name="description" content="Lingfield Marathon Fun Run">
+  <meta name="author" content="Pete Tavener">
+  
+  <!-- for use when results arriving -->
+  <meta http-equiv="refresh" content="120">
+  
+  <MECSS/desktop.cssTA HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+  
+   <script src="JS/modernizr.js"></script>
+   <script type="text/javascript"
+src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+
+  
+  
+  <link rel="icon" href="http://www.lingfield-marathon.org/favicon.ico" type="image/x-icon" /> 
+<link rel="icon" href="favicon.ico"> 
+  
+
+<link rel=stylesheet href="CSS/desktop.css" type="text/css" />
+<link media="screen and (max-width: 800px)" rel="stylesheet"  type="text/css" href="CSS/mobile.css" >
+</head>
+<body >
+
+ 
+<div id="page">
+
+<div id="header">
+<?php include("Includes/header.php"); ?>
+</div>
+
+<div id="nav">
+<?php include("HorizontalSubnav/navbar.php"); ?>
+</div>
+
+<div id="filmstrip"> 
+<?php include("Includes/filmstrip.php"); ?> 
+</div> 
+
+<div id="blurb">
+</br></br></br>
+
+  <div class="list">
+	  
+	  <h1 align="left">Grants awarded 2013</h1>
+        
+        <ul>
+        <li>Young Epilepsy</li>
+        <li>Surrey Raynet</li>
+        <li>Victoria Sports & Social Club</li>
+        <li>1st Lingfield & Dormansland Scout Group</li>
+        <li>Lingfield Marathon Minibus</li>
+        <li>Action for Life (walking groups)</li>
+        <li>Blonde McIndoe</li>
+        <li>Dormans Rockets FC</li>
+        <li>Disability Access EG Area</li>
+        <li>Haven Group</li>
+        <li>Lingfield Art group</li>
+        <li>RH7 History Group</li>
+        <li>St Catherines Hospice</li>
+        </ul>
+        
+        
+        </div>
+  <br/><br/><br/>
+    
+	<p>Congratulations to all of you for your successful applications. We'd be really happy to hear how you used the money and would love to share your stories with other website readers. Make sure you keep us posted and we look forward to hearing from you.</p>
+	   
+ 	 
+
+</div>
+
+<div id="adverts_include">
+<?php include("Adverts/adverts.php");  ?>
+</div>
+
+  
+<div id="footer">
+<?php include("Includes/footer.php"); ?>
+</div>
+
+    
+</div id="page">
+
+</body>
+
+</html>
