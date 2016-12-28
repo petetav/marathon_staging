fafa126e8c2bd350b0e1541998efbb307fd9a361
petetav <petethepilot69@gmail.com>
2016-12-28 08:26:19 +0000

    first attempt

---
+++ contact_us.php
@@ -0,0 +1,125 @@
+<html>
+<head>
+<meta charset="utf-8">
+
+<title>Lingfield Marathon Fun Run 2014</title>
+ <meta name="description" content="Lingfield Marathon Fun Run">
+  <meta name="author" content="Pete Tavener">
+  
+  <!-- for use when results arriving -->
+  <meta http-equiv="refresh" content="120">
+  
+  <MECSS/desktop.cssTA HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+  
+   <script src="JS/modernizr.js"></script>
+   <script type="text/javascript"
+src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+
+  
+  
+  <link rel="icon" href="http://www.lingfield-marathon.org/favicon.ico" type="image/x-icon" /> 
+<link rel="icon" href="favicon.ico"> 
+  
+
+<link rel=stylesheet href="CSS/desktop.css" type="text/css" />
+<link media="screen and (max-width: 800px)" rel="stylesheet"  type="text/css" href="CSS/mobile.css" >
+</head>
+<body >
+
+ 
+ 
+<div id="page">
+
+<div id="header">
+<?php include("Includes/header.php"); ?>
+</div>
+
+<div id="nav">
+<?php include("HorizontalSubnav/navbar.php"); ?>
+</div>
+
+<div id="filmstrip"> 
+<?php include("Includes/filmstrip.php"); ?> 
+</div> 
+
+<div id="blurb">
+
+ 	
+  </br></br></br></br> 
+        <h1 align="center">The Committee</h1>
+<br/></br></br>
+	  
+      <div align="center">
+        <table class="contacts" border="0" cellpadding="0" width="90%">
+          <tr>
+            <td width="166"><div align="left">Chairman</div></td>
+            <td width="195"><div align="left">Graham Marks</div></td>
+            <td width="160"><div align="right">01342 832529 </div></td>
+          </tr>
+          <tr>
+            <td width="166"><div align="left">Treasurer &amp; Stalls</div></td>
+            <td width="195"><div align="left"><a href="mailto:mike.bidbury@btinternet.com">Michael Ellis</a> </div></td>
+            <td width="160"><div align="right">01342 870261 </div></td>
+          </tr>
+          <tr>
+            <td width="166"><div align="left">Secretary</div></td>
+            <td width="195"><div align="left"><a href="mailto:sue@bidbury.com">Sue Ellis</a> </div></td>
+            <td width="160"><div align="right">01342 870261 </div></td>
+          </tr>
+          <tr>
+            <td width="166"><div align="left">&nbsp;</div></td>
+            <td width="195"><div align="left"><a href="mailto:VaughanJillH@aol.com">Jill Harris</a></div></td>
+            <td width="160"><div align="right">01342 833606 </div></td>
+          </tr>
+         
+          <tr>
+            <td width="166"><div align="left">&nbsp;</div></td>
+            <td width="195"><div align="left"><a href="mailto:brianspicer@btinternet.com ">Brian Spicer</a><a href="mailto:Jonathan.hall@hp.com "></a></div></td>
+            <td width="160"><div align="right">01883 715566</div></td>
+          </tr>
+          <tr>
+            <td width="166"><div align="left">&nbsp;</div></td>
+            <td width="195"><div align="left"><a href="mailto:Gordon.hyde@tiscali.co.uk">Gordon Hyde</a><a href="mailto:brian@spicer.orangehome.co.uk "></a></div></td>
+            <td width="160"><div align="right">01342 326391</div></td>
+          </tr>
+          <tr>
+           <td width="166"><div align="left">&nbsp;</div></td>
+            <td width="195"><div align="left"><a href="mailto:davew@albert-ross.co.uk">Dave Wilkes</a><a href="mailto:davew@albert-ross.co.uk "></a></div></td>
+            <td width="160"><div align="right"></div></td>
+          </tr>
+          
+          <tr>
+           <td width="166"><div align="left">&nbsp;</div></td>
+            <td width="195"><div align="left"><a href="mailto:claire-mcdonald1@hotmail.co.uk">Scott McDonald</a></a></div></td>
+            <td width="160"><div align="right">01342 835531</div></td>
+          </tr>
+          
+          <tr>
+            <td width="166"><div align="left">Webmaster</div></td>
+            <td width="195"><div align="left"><a href="mailto:webmaster@lingfield-marathon.org">Pete Tavener </a></div></td>
+            <td width="160"><div align="right">+33 450 38 70 93</div></td>
+          </tr>
+        </table>
+    </div>
+</br></br></br></br></br>
+	  <p>Do feel free to contact any of us, preferably by email if possible.&nbsp; We look forward to hearing from you. </p>
+    
+ 	 
+
+</div>
+
+<div id="adverts_include">
+<?php include("Adverts/adverts.php");  ?>
+</div>
+
+  
+<div id="footer">
+<?php include("Includes/footer.php"); ?>
+</div>
+
+    
+</div id="page">
+
+</body>
+
+</html>
